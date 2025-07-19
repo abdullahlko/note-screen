71ee7b0478e913f2802a478ea4cb456274d48cf4
Abdullah Ansari <abdullahlko789@gmail.com>
2025-07-19 19:44:15 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,8 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer className="text-center text-sm text-gray-500 py-4 mt-10 border-t border-gray-300">
-      © {year} <span className="font-semibold text-gray-700">TodoFlow</span> by{" "}
+      © {year} <span className="font-semibold text-gray-700">NoteScreen</span>{" "}
+      by{" "}
       <a
         href="https://www.linkedin.com/in/abdullahlko"
         target="_blank"
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
     <nav className="flex justify-center items-center bg-[#f5ba13] text-white p-4 py-5">
       <div className="logo flex items-center gap-3">
         <TipsAndUpdatesIcon style={{ fontSize: "4rem", color: "white" }} />
-        <span className="font-extrabold text-4xl">TodoFlow</span>
+        <span className="font-extrabold text-4xl">NoteScreen</span>
       </div>
     </nav>
   );
